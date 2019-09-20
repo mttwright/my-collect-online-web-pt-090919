a7300e3528838b27c7a0cff9daba285b125cc5c0
virtual-frame-7789 <mttwright@gmail.com>
2019-09-20 21:52:16 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(array)
+  counter = 0
+  return_array = []
+  while counter < array.length
+    return_array.push(yield(array[counter]))
+    counter += 1
+  end
+  return_array
+end
 
 
